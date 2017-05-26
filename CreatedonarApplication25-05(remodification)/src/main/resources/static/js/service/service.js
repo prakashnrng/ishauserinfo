dab5313695cb71774f41400e916c9b0187ee52ab
Meghanahs <meghasrinivas122@gmail.com>
2017-05-26 11:40:33 +0530

    Update service.js

--- CreatedonarApplication25-05(remodification)/src/main/resources/static/js/service/service.js
+++ CreatedonarApplication25-05(remodification)/src/main/resources/static/js/service/service.js
@@ -2,14 +2,15 @@
 myApp.factory("localService",['$http','$log','$q',function($http,$log,$q){
 	var factory = {
 			register : registration	,
-			fetchDonors  : fetchDonors
+			fetchDonors : fetchDonors,
+			createDonor: createDonor
 	};
 	return factory;
 	
 	function registration(registerData){
 		var deferred = $q.defer();
 		$log.log("---Register servivce entered---");
-		$http.post('http://localhost:6512/hello1/',registerData)
+		$http.post('http://localhost:6511/hello1/',registerData)
 		.then(function(SuccesResponse){
 			$log.log("----Registerd Successfully----");
 			deferred.resolve(SuccesResponse.data);
@@ -23,7 +24,7 @@ myApp.factory("localService",['$http','$log','$q',function($http,$log,$q){
 	};
 	
 	function fetchDonors(){
-		return $http.get("http://localhost:6512/hello1/")
+		return $http.get("http://localhost:6511/hello1/")
 		.then(function(fetchResponse){
 			return fetchResponse.data;
 			
@@ -33,5 +34,27 @@ myApp.factory("localService",['$http','$log','$q',function($http,$log,$q){
 		}
 		);
 	};
+	
+	function createDonor(donar_info) {
+	     console.log('Creating donor');
+	     var deferred = $q.defer();
+	     console.log('-------------------creteDonor service--------------');
+	  
+	     console.log(donar_info);
+	     $http.post('http://localhost:6511/donar1/',donar_info)
+	         .then(
+	             function (response) {
+	            	 console.log('response block');
+	            	 console.log(donar_info);
+	            //	 loadAlldonor();
+	                 deferred.resolve(response.data);
+	             },
+	             function (errResponse) { 
+	                console.error('Error while creating Donor :');
+	                deferred.reject(errResponse);
+	             }
+	         );
+	     return deferred.promise;
+	 }
 
 }]);
